3d6cb9e47d99573412e93f1c336f8ce435ef6257
HNK-GIC <126233679+HNK-GIC@users.noreply.github.com>
2023-07-03 13:13:35 +0630

    initialize

---
+++ app/Http/Controllers/API/PersonController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $all_p = DB::table('persons')
+                    ->select('*')->get();
+        return $all_p;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $p = new Person();
+        $p->reg_no = $request->reg_no;
+        $p->name = $request->name;
+        $p->dob = $request->dob;
+        $p->address = $request->address;
+        $p->phone = $request->phone;
+        $p->save();
+        
+        return $p;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
